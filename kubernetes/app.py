cdd4722655d908d4c2a57c928e378b4986ad33d9
Vinta <vinta.chen@gmail.com>
2018-02-15 03:11:15 +0800

    flask v2

--- kubernetes/app.py
+++ kubernetes/app.py
@@ -1,3 +1,6 @@
+import datetime
+import platform
+
 from flask import Flask
 
 
@@ -6,9 +9,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index'
+    hostname = platform.node()
+    now = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    return f'You hit "{hostname}" at {now}'
 
 
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
